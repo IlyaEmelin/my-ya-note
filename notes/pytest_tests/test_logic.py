7ba81def0488e5e38e9f13f6037be2053815b737
Илья <bkmz8_133@mail.ru>
2025-06-03 18:21:40 +0300

    all

--- notes/pytest_tests/test_logic.py
+++ notes/pytest_tests/test_logic.py
@@ -4,6 +4,7 @@ import pytest
 from pytest_django.asserts import assertRedirects, assertFormError
 from django.urls import reverse
 from pytils.translit import slugify
+from http import HTTPStatus
 
 from notes.models import Note
 from notes.forms import WARNING
@@ -62,3 +63,46 @@ def test_empty_slug(author_client, form_data):
     expected_slug = slugify(form_data["title"])
     # Проверяем, что slug заметки соответствует ожидаемому:
     assert new_note.slug == expected_slug
+
+
+def test_author_can_edit_note(author_client, form_data, note):
+    """Только автор может редактировать заметку"""
+    url = reverse("notes:edit", args=(note.slug,))
+
+    response = author_client.post(url, form_data)
+
+    assertRedirects(response, reverse("notes:success"))
+    note.refresh_from_db()
+    assert note.title == form_data["title"]
+    assert note.text == form_data["text"]
+    assert note.slug == form_data["slug"]
+
+
+def test_other_user_cant_edit_note(not_author_client, form_data, note):
+    """Нельзя редактировать чужую заметку"""
+    url = reverse("notes:edit", args=(note.slug,))
+
+    response = not_author_client.post(url, form_data)
+
+    assert response.status_code == HTTPStatus.NOT_FOUND
+    note_from_db = Note.objects.get(id=note.id)
+    # Проверяем, что атрибуты объекта из БД равны атрибутам заметки до запроса.
+    assert note.title == note_from_db.title
+    assert note.text == note_from_db.text
+    assert note.slug == note_from_db.slug
+
+
+def test_author_can_delete_note(author_client, slug_for_args):
+    """Автор может удалить свою заметку"""
+    url = reverse("notes:delete", args=slug_for_args)
+    response = author_client.post(url)
+    assertRedirects(response, reverse("notes:success"))
+    assert Note.objects.count() == 0
+
+
+def test_other_user_cant_delete_note(not_author_client, slug_for_args):
+    """Другой пользователь не может удалить чужую заметку"""
+    url = reverse("notes:delete", args=slug_for_args)
+    response = not_author_client.post(url)
+    assert response.status_code == HTTPStatus.NOT_FOUND
+    assert Note.objects.count() == 1
