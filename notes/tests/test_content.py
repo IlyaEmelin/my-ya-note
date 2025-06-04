b5fb47b92ae5b89dd17296c68fd260f092063e8a
Илья <bkmz8_133@mail.ru>
2025-06-04 10:24:27 +0300

    test content

--- notes/tests/test_content.py
+++ notes/tests/test_content.py
@@ -2,6 +2,7 @@ from django.db.models import QuerySet
 from django.urls import reverse
 from django.contrib.auth import get_user_model
 from django.test import TestCase
+from django.test.client import Client
 
 from notes.models import Note
 
@@ -11,65 +12,64 @@ User = get_user_model()
 class TestNotesList(TestCase):
     """Тестирование страницы списка заметок"""
 
-    NOTES_LIST_URL = reverse("notes:list")
-    # так как нет не сортировки, не пейджинга
-    # будем тестировать на 100-а заметок
-    COUNT_NOTE = 100
-
-    @classmethod
-    def __gen_note(cls):
-        """Генератор тестовых данных"""
-        for i in range(cls.COUNT_NOTE):
-            yield (
-                ("title", f"Заметка {i}"),
-                ("text", f"Текст заметки {i}"),
-                ("slug", f"slug_{i}"),
-                ("author", cls.author),
-            )
-
     @classmethod
     def setUpTestData(cls):
+        cls.notes_list_url = reverse("notes:list")
+
         cls.author = User.objects.create(username="Серега Пушкин")
+        cls.author_client = Client()
+        cls.author_client.force_login(cls.author)
+
         cls.user = User.objects.create(username="Иван Евтушенко")
+        cls.user_client = Client()
+        cls.user_client.force_login(cls.user)
 
-        all_notes = [Note(**dict(note_args)) for note_args in cls.__gen_note()]
-        Note.objects.bulk_create(all_notes)
+        # all_notes = [Note(**dict(note_args)) for note_args in cls.__gen_note()]
+        # Note.objects.bulk_create(all_notes)
+        cls.note = Note.objects.create(
+            title="Заголовок",
+            text="Текст заметки",
+            slug="note-slug",
+            author=cls.author,
+        )
 
-    def test_author_notes(self):
-        """Тестирование списка заметок автора"""
-        self.client.force_login(self.author)
-        response = self.client.get(self.NOTES_LIST_URL)
-        notes = response.context.get("object_list")
+    def test_notes_list_for_different_users(self):
+        """Тестирование списка заметок для разных авторов"""
+        for client, note_in_list in (
+            (self.author_client, True),
+            (self.user_client, False),
+        ):
+            with self.subTest(client=client, note_in_list=note_in_list):
+                response = client.get(self.notes_list_url)
+                notes = response.context.get("object_list")
 
-        self.assertIsInstance(notes, QuerySet)
-        self.assertEqual(len(notes), self.COUNT_NOTE)
-        data_tests = set(
-            (
-                ("title", note.title),
-                ("text", note.text),
-                ("slug", note.slug),
-                ("author", note.author),
+                self.assertIsInstance(
+                    notes,
+                    QuerySet,
+                    msg=(
+                        "Список заметок должен быть "
+                        "соответствующего типа данных"
+                    ),
+                )
+                self.assertEqual(
+                    self.note in notes,
+                    note_in_list,
+                    msg="Не корректно видит список заметок",
+                )
+
+    def test_pages_contains_form(self):
+        """Тестирование форм создания и редактирования заметки"""
+        for name, args in (
+            ("notes:add", None),
+            ("notes:edit", (self.note.slug,)),
+        ):
+            response = self.author_client.get(reverse(name, args=args))
+
+            self.assertIn(
+                "form",
+                response.context,
+                msg=(
+                    "на страницы создания и редактирования заметки "
+                    "передаются формы."
+                ),
             )
-            for note in notes
-        )
-        self.assertEqual(
-            data_tests,
-            set(self.__gen_note()),
-            msg=(
-                "Или не все данные добавлены в базу "
-                "или не все данные получены из базы"
-            ),
-        )
-
-    def test_user_notes(self):
-        """Тестирование списка заметок автора"""
-        self.client.force_login(self.user)
-        response = self.client.get(self.NOTES_LIST_URL)
-        notes = response.context.get("object_list")
-
-        self.assertIsInstance(notes, QuerySet)
-        self.assertEqual(
-            len(notes),
-            0,
-            msg="Пользователь видит не свои заметки",
-        )
